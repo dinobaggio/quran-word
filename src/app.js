c81a74179dcfacafec91fb3f07547904cfaf827b
dino <dbaggio111@gmail.com>
2022-06-24 22:25:08 +0700

    feat: first init

---
+++ src/app.js
@@ -0,0 +1,39 @@
+import createError from 'http-errors'
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import indexRouter from './routes/index'
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.status(404).json({ message: 'not found' })
+  // next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  console.error(err)
+
+  res.status(500).render('error', { message: err?.message, error: JSON.stringify(err, null, 2) });
+});
+
+export default app;
