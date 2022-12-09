61b98e04331079264efe7c156669b1a8686c53ae
dino <dbaggio111@gmail.com>
2022-12-09 16:03:00 +0700

    commit

--- tests/usersAmqpTest.js
+++ tests/usersAmqpTest.js
@@ -28,36 +28,36 @@ if (ACTIVE_RABBITMQ === 'no') {
   })
 } else if (ACTIVE_RABBITMQ === 'yes') {
   describe('[RabbitMQ] Test users amqp', () => {
-    // it('[Create user] success /', async () => {
-    //   const { body: login } = await chai.request(app).post(LOGIN_ENDPOINT)
-    //     .send(CREDENTIALS_LOGIN)
-    //   const res = await chai.request(app).post(ENDPOINT)
-    //     .set('Authorization', `Bearer ${login.token}`)
-    //     .send({
-    //       name: faker.name.fullName(),
-    //       email: faker.internet.email(),
-    //       password: 'password'
-    //     })
-    //   res.should.have.status(201)
-    //   res.body.should.have.own.property('message')
-    // })
-    // it('[Update user] success /', async () => {
-    //   const user = await create({
-    //     name: faker.name.fullName(),
-    //     email: faker.internet.email(),
-    //     password: 'password'
-    //   })
-    //   const updateName = faker.name.fullName()
-    //   const { body: login } = await chai.request(app).post(LOGIN_ENDPOINT)
-    //     .send(CREDENTIALS_LOGIN)
-    //   const res = await chai.request(app).put(`${ENDPOINT}/${user.uuid}`)
-    //     .set('Authorization', `Bearer ${login.token}`)
-    //     .send({
-    //       name: updateName
-    //     })
-    //   res.should.have.status(200)
-    //   res.body.should.have.own.property('message')
-    // })
+    it('[Create user] success /', async () => {
+      const { body: login } = await chai.request(app).post(LOGIN_ENDPOINT)
+        .send(CREDENTIALS_LOGIN)
+      const res = await chai.request(app).post(ENDPOINT)
+        .set('Authorization', `Bearer ${login.token}`)
+        .send({
+          name: faker.name.fullName(),
+          email: faker.internet.email(),
+          password: 'password'
+        })
+      res.should.have.status(201)
+      res.body.should.have.own.property('message')
+    })
+    it('[Update user] success /', async () => {
+      const user = await create({
+        name: faker.name.fullName(),
+        email: faker.internet.email(),
+        password: 'password'
+      })
+      const updateName = faker.name.fullName()
+      const { body: login } = await chai.request(app).post(LOGIN_ENDPOINT)
+        .send(CREDENTIALS_LOGIN)
+      const res = await chai.request(app).put(`${ENDPOINT}/${user.uuid}`)
+        .set('Authorization', `Bearer ${login.token}`)
+        .send({
+          name: updateName
+        })
+      res.should.have.status(200)
+      res.body.should.have.own.property('message')
+    })
     it('[Delete user] success /', async () => {
       const user = await create({
         name: faker.name.fullName(),
